498ddc6c582f867a4565e4019bd8fae056723bf1
elbenton <54687566+elbenton@users.noreply.github.com>
2019-11-25 10:26:42 -0600

    Add files via upload

--- dogappv3/script.js
+++ dogappv3/script.js
@@ -1,31 +1,46 @@
 'use strict';
 
 function getDogImage() {
-  fetch('https://dog.ceo/api/breed/hound/images/random')
+  fetch("https://dog.ceo/api/breed/" + handleUserInput() + "/images/random")
     .then(response => response.json())
-    .then(responseJson => 
-      displayResults(responseJson))
+    .then(responseJson => displayResults(responseJson))
     .catch(error => alert('A dog ate your homework. Try again!'));
 }
 
 function watchForm() {
-  $("#formNumberOfDogsId").submit(event => {
+ $("#formInput").submit(event => {
     event.preventDefault();
     getDogImage();
   });
 }
 
-function displayResults(responseJson) {
-  console.log(responseJson);
-  //replace the existing image with the new one
-  $('.dog-img').replaceWith(
-    `<img src="${responseJson.message}" class="dog-img">`
-  )
-  //display the results section
-  $('.results').show()
+function handleUserInput() {
+  let breed = $("#inputBreedId").val();
+  return breed;
 }
 
-$('.hidden').hide()
+
+
+function displayResults(responseJson) {
+  console.log(responseJson);
+  if (responseJson.status !== 'success') {
+    alert("We don't have that breed...yet");
+  } else if (responseJson.status === 'success') {
+    $("#results").replaceWith(
+      '<div id="results" class="container">'
+     + '<section class="results hidden">'
+     +   '<h3>It\'s a dog!</h3>'
+     + '<img src="'+responseJson.message+'" class="results">'
+     + '  </section>'
+     + '</div>'
+    );
+    $(".results").removeClass("hidden");
+    console.log('Image replaced with '+responseJson.message);
+  }
+}
+
+$('.hidden').hide();
+
 $(function() {
   console.log('App loaded! Waiting for submit!');
   watchForm();
